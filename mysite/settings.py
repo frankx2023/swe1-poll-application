e898d756c237334e66bcc2f29e3e455255bf613e
Frankx <117894913+frankx2023@users.noreply.github.com>
2025-02-13 19:18:23 -0500

    updates for eb

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^cgv5-6=a15i1=7s6*wt8b0v5g1zct3wm55clzvef_=q15%q^*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.eba-k9pvgbqm.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
